75914255e4f0f266f67d4812446a1002ff3e10bf
Max Black <maxblackcase@gmail.com>
2020-07-09 14:02:48 +0400

    Тестирование модели представления

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels {
+
+  internal class MainWindowViewModel : ViewModel {
+
+    private string _Title = "Анализ статистики";
+
+    #region Заголовок окна
+
+    /// <summary>Заголовок окна</summary>
+    public string Title {
+      get => _Title;
+      //if ( Equals( _Title, value ) )
+      //  return;
+
+      set => Set( ref _Title, value );
+    }
+    #endregion 
+  }
+}
