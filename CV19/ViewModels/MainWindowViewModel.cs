e76412c11d5a57c309168848ff7883488c6062c3
Max Black <maxblackcase@gmail.com>
2020-07-10 10:16:30 +0400

    Тестируем использование элементов WPF в библиотеках

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -16,6 +16,14 @@ namespace CV19.ViewModels {
 
       set => Set( ref _Title, value );
     }
-    #endregion 
+    #endregion
+
+    #region Status : string - Статус программы
+
+    private string _Status = "Готов!";
+
+    public string Status { get => _Status; set => Set( ref _Status, value ); }
+
+    #endregion
   }
 }
